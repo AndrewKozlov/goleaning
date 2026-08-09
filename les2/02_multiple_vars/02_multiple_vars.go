80181eaa298b6925116a7e2a6c42b721118fe5f3
AndrewKozlov <koclov@yandex.ru>
2026-08-09 23:38:39 +0300

    opapap

--- les2/02_multiple_vars/02_multiple_vars.go
+++ les2/02_multiple_vars/02_multiple_vars.go
@@ -9,7 +9,7 @@ func main() {
 
 	price := 2.50
 
-	fmt.Println("Small Espresso price is $2.50")
+	fmt.Println("Small Espresso price is $2.60")
 
 	fmt.Println(size, coffeeName, "price is $", price)
 }
